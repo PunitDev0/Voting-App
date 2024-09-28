f41078669d64ad56466b5f2d7973054a4c8f1e68
PunitDev0 <punitdeveloper1@tgmail.com>
2024-09-28 21:19:19 +0530

    Fixed some bugs

--- Jwt.js
+++ Jwt.js
@@ -1,24 +1,33 @@
 import jwt from 'jsonwebtoken';
 
-const jwtAuthMiddleware = (req,res,next)=>{
-    const token = req.headers.authorization.spli(' ')[1]
-    if(!token) return res.status(401).json({error : 'Invalid token'})
-        try{
+const jwtAuthMiddleware = (req, res, next) => {
 
-            const decoded = jwt.verify(token,process.env.JWT_SECRET);
-            req.user = decoded
-            next()
+    // first check request headers has authorization or not
+    const authorization = req.headers.authorization
+    if(!authorization) return res.status(401).json({ error: 'Token Not Found' });
 
+    // Extract the jwt token from the request headers
+    const token = req.headers.authorization.split(' ')[1];
+    if(!token) return res.status(401).json({ error: 'Unauthorized' });
 
-        }catch(error){
-            console.error(error)
-            return res.status(401).json({error : 'Token is not valid'})
-        }
+    try{
+        // Verify the JWT token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
+        // Attach user information to the request object
+        req.user = decoded
+        next();
+    }catch(err){
+        console.error(err);
+        res.status(401).json({ error: 'Invalid token' });
+    }
 }
 
-const generateToken = (userData) =>{
-    return jwt.sign(userData,process.env.JWT_SECRET,{expiresIn : '1h'}) 
+
+// Function to generate JWT token
+const generateToken = (userData) => {
+    // Generate a new JWT token using user data
+    return jwt.sign(userData, process.env.JWT_SECRET, {expiresIn: 30000});
 }
 
-export  {jwtAuthMiddleware, generateToken}
+export {jwtAuthMiddleware, generateToken};
